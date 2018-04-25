f2eb1e2ef910cbb911cb82cbc2985a72a3b3629c
ZhengSheng <zhengsheng0118@163.com>
2018-04-25 16:26:57 +0000

    finished

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,18 +2,31 @@
 
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Auth;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Traits\LastActivedAtHelper;
     use Traits\ActiveUserHelper;
     use HasRoles;
     use Notifiable {
         notify as protected laravelNotify;
+    }
+
+    //Rest omitted for brevity
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 
     public function notify($instance)
