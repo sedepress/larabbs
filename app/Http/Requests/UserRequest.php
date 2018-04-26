25c2889c86feeba7cc46c16f8c985288bf2290ce
zs <zhengsheng0118@163.com>
2018-04-26 06:18:26 +0000

    user update

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -14,11 +14,33 @@ class UserRequest extends FormRequest
 
     public function rules()
     {
+        switch($this->method()) {
+            case 'POST':
+                return [
+                    'name' => 'required|string|max:255',
+                    'password' => 'required|string|min:6',
+                    'verification_key' => 'required|string',
+                    'verification_code' => 'required|string',
+                ];
+                break;
+            case 'PATCH':
+                $userId = \Auth::guard('api')->id();
+                return [
+                    'name' => 'between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' .$userId,
+                    'email' => 'email',
+                    'introduction' => 'max:80',
+                    'avatar_image_id' => 'exists:images,id,type,avatar,user_id,'.$userId,
+                ];
+                break;
+        }
+    }
+
+    public function attributes()
+    {
         return [
-            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
-            'email' => 'required|email',
-            'introduction' => 'max:80',
-            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=200,min_height=200',
+            'verification_key' => '短信验证码 key',
+            'verification_code' => '短信验证码',
+            'introduction' => '个人简介',
         ];
     }
 
