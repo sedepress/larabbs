4e3bde46f060775d9ca4e20248575d0ba57a59b7
ZhengSheng <zhengsheng0118@163.com>
2018-04-26 14:35:46 +0000

    push notification

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,7 +46,7 @@ class User extends Authenticatable implements JWTSubject
      */
     protected $fillable = [
         'name', 'phone', 'email', 'password', 'introduction', 'avatar',
-        'weixin_openid', 'weixin_unionid'
+        'weixin_openid', 'weixin_unionid', 'registration_id'
     ];
 
     /**
